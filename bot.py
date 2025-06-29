66e5996dd63d53f1816e2861c77eff36cbcd8dc8
Simisticot <simi.pra@gmail.com>
2025-06-29 22:42:41 +0200

    basics with flask server to retrieve the credentials

--- bot.py
+++ bot.py
@@ -1,30 +1,118 @@
 # This example requires the 'message_content' intent.
 
+import json
+import re
+import googleapiclient.discovery
+import google.oauth2.credentials
 import discord
+from discord.ext import commands
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+
+CREDS = google.oauth2.credentials.Credentials(
+    **json.loads(open("creds.json", "r").read())
+)
+
+watched_channels: set[int] = set()
+
+bot = commands.Bot(command_prefix="$", intents=intents)
+
+youtube_re = re.compile(
+    r"((?:https?:)?\/\/)?((?:www|m)\.)?((?:youtube(-nocookie)?\.com|youtu.be))(\/(?:[\w\-]+\?v=|embed\/|v\/)?)([\w\-]+)(\S+)?"
+)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    print(f"logged in as {bot.user}")
 
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
+@bot.event
+async def on_message(message: discord.Message):
+    if message.content.startswith("$"):
+        await bot.process_commands(message)
+    if message.author.id == bot.user.id:
+        print("message from self")
+    if message.guild is None:
+        print("invalid channel")
         return
+    if message.guild.id not in watched_channels:
+        print("not a watched channel")
+        return
+    matches = youtube_re.findall(message.content)
+    if len(matches) == 0:
+        print("no matches")
+        return
+    else:
+        youtube = googleapiclient.discovery.build("youtube", "v3", credentials=CREDS)
+        for m in matches:
+            youtube.playlistItems().insert(
+                part="snippet",
+                body={
+                    "snippet": {
+                        "playlistId": "PLVKgx_siHkYG6eRYnnvUlZzWIURV5qeJH",
+                        "position": 0,
+                        "resourceId": {"kind": "youtube#video", "videoId": m[5]},
+                    }
+                },
+            ).execute()
+            await message.channel.send(f"added {m[5]} to the playlist")
 
-    if message.content.startswith("$hello"):
-        await message.channel.send("Hello!")
+
+@bot.command()
+async def watch(ctx: commands.Context):
+    if ctx.guild is None:
+        await ctx.send("this isn't a channel I can watch")
+        return
+    channel_id = ctx.guild.id
+    watched_channels.add(channel_id)
+    await ctx.send(f"watching channel {channel_id}")
+
+
+@bot.command()
+async def watched(ctx: commands.Context):
+    await ctx.send(
+        f"watched channels: {", ".join(str(channel) for channel in watched_channels)}"
+    )
+
+
+@bot.command()
+async def add(ctx: commands.Context, id: str):
+    await ctx.send(f"adding video {id}")
+    youtube = googleapiclient.discovery.build("youtube", "v3", credentials=CREDS)
+    youtube.playlistItems().insert(
+        part="snippet",
+        body={
+            "snippet": {
+                "playlistId": "PLVKgx_siHkYG6eRYnnvUlZzWIURV5qeJH",
+                "position": 0,
+                "resourceId": {"kind": "youtube#video", "videoId": id},
+            }
+        },
+    ).execute()
+    await ctx.send("Done !")
+
+
+@bot.command()
+async def rename(ctx: commands.Context, name: str):
+    await ctx.send(f"renaming to {name}")
+    youtube = googleapiclient.discovery.build("youtube", "v3", credentials=CREDS)
+    youtube.playlists().update(
+        part="snippet,status",
+        body={
+            "id": "PLVKgx_siHkYG6eRYnnvUlZzWIURV5qeJH",
+            "snippet": {
+                "title": name,
+            },
+        },
+    ).execute()
 
 
 token = os.environ.get("DISCORD_TOKEN")
 if token is None:
     print("Please provide a discord token in environment variable 'DISCORD_TOKEN'")
 else:
-    client.run(token)
+    bot.run(token)
---
+++ flask_server.py
@@ -0,0 +1,102 @@
+import os
+import flask
+import requests
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+
+SECRETS_FILE = "client_secret.json"
+SCOPES = [
+    "https://www.googleapis.com/auth/youtubepartner",
+    "https://www.googleapis.com/auth/youtube",
+    "https://www.googleapis.com/auth/youtube.force-ssl",
+]
+
+app = flask.Flask(__name__)
+app.secret_key = "yipeeeeeee"
+
+
+@app.route("/")
+def root():
+    return "Hi"
+
+
+@app.route("/make_cool_playlist")
+def make_cool_playlist():
+    if "creds" not in flask.session:
+        return flask.redirect("authorize")
+    else:
+        creds = google.oauth2.credentials.Credentials(**flask.session["creds"])
+        youtube = googleapiclient.discovery.build("youtube", "v3", credentials=creds)
+        youtube.playlists().insert(
+            part="snippet,status",
+            body={
+                "snippet": {
+                    "title": "MY VERY COOL PLAYLIST",
+                    "description": "made via playlisticot",
+                    "tags": ["sample playlist", "API call"],
+                    "defaultLanguage": "en",
+                },
+                "status": {"privacyStatus": "private"},
+            },
+        ).execute()
+    return "did it work ?"
+
+
+@app.route("/authorize")
+def authorize():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        SECRETS_FILE, scopes=SCOPES
+    )
+    flow.redirect_uri = flask.url_for("callback", _external=True)
+    authorization_url, state = flow.authorization_url(
+        access_type="offline", include_granted_scopes="true"
+    )
+    flask.session["state"] = state
+
+    return flask.redirect(authorization_url)
+
+
+@app.route("/callback")
+def callback():
+    state = flask.session["state"]
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        SECRETS_FILE, scopes=SCOPES, state=state
+    )
+    flow.redirect_uri = flask.url_for("callback", _external=True)
+
+    authorization_response = flask.request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    creds = flow.credentials
+
+    flask.session["creds"] = credentials_to_dict(creds)
+
+    return flask.redirect(flask.url_for("make_cool_playlist"))
+
+
+def credentials_to_dict(credentials):
+    return {
+        "token": credentials.token,
+        "refresh_token": credentials.refresh_token,
+        "token_uri": credentials.token_uri,
+        "client_id": credentials.client_id,
+        "client_secret": credentials.client_secret,
+        "granted_scopes": credentials.granted_scopes,
+    }
+
+
+if __name__ == "__main__":
+    # When running locally, disable OAuthlib's HTTPs verification.
+    # ACTION ITEM for developers:
+    #     When running in production *do not* leave this option enabled.
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+
+    # This disables the requested scopes and granted scopes check.
+    # If users only grant partial request, the warning would not be thrown.
+    os.environ["OAUTHLIB_RELAX_TOKEN_SCOPE"] = "1"
+
+    # Specify a hostname and port that are set as a valid redirect URI
+    # for your API project in the Google API Console.
+    app.run("localhost", 8800, debug=True)
